b9325b862cf26519a0e3cb5619f5768c10c60291
Keshuac <48709680+Keshuac@users.noreply.github.com>
2022-05-04 00:26:33 +1200

    Removed Test Code

--- Ciphers/columnar.py
+++ Ciphers/columnar.py
@@ -30,11 +30,7 @@ def columnarSingleThreaded(cipherText:str,maxKeyLength:int=9):
 
 def columnarMultiThreaded(cipherText:str,maxKeyLength:int=9):
     perms = ['']
-    # totalMult = 0
-    # total = 0
     for i in tqdm(range(1, maxKeyLength+1),ascii=globals.asciiFallback,desc='BruteForce Progress'):
-        # if globals.verbose:
-        #     print('Key Length n = %d' % i)
         perms = [''.join(p) for p in permutations(perms[0]+str(i))]        
         with Pool(cpu_count()) as p:
             """
@@ -46,38 +42,7 @@ def columnarMultiThreaded(cipherText:str,maxKeyLength:int=9):
             however during testing, single-threading was faster until n=9 where multi-threading was overwhelmingly faster
             """
             globals.ranked += p.map(partial(scoreViaDict,dic=globals.dic),bruteForceDecipherList(cipherText,perms))
-            
-            '''
-            NOTE Testing Code
 
-            start = timer()
-            temp = p.map(partial(bruteForceDecipherMulti,cipherText),perms)
-            end = timer()
-            print(f'elapsed time(bruteforceMulti): {end-start}')
-            totalMult += end-start
-
-            start = timer()
-            temp1 = 
-            end = timer()
-            print(f'elapsed time(Scoring): {end-start}')
-            total += end-start
-            
-            if (temp1!=temp):
-                print('ERROR')
-                input()
-            
-            globals.ranked = []
-            start = timer()
-            end = timer()
-
-            #TESTING
-            print(temp1==globals.ranked)
-            print(temp1)
-            print(globals.ranked)
-            input()
-    print("Total (MultiThreaded): {}".format(totalMult))
-    print("Total (SingleThreaded): {}".format(total))
-    '''
 
 def bruteForceDecipher(cipherText, perms):
     for key in perms:
@@ -107,39 +72,5 @@ def bruteForceDecipherList(cipherText, perms) -> list:
     return returnList
 
 
-
-# cipherText = ""
-# index = 0
-# if __name__ == '__main__':
-#     globals.verbose = True
-#     importWords('..\Dictionaries\\1000TopDict.txt')
-#     cipherText = "MNHERZRNTDHZAAUINEEYSDEZ"
-#     columnarMultiThreaded(cipherText)
-#     total = 0
-#     for i in range(3):
-#         start = timer()
-
-#         columnarMultiThreaded(cipherText)
-#         end = timer()
-#         total += end - start
-#     mulithreaded = total
-    
-#     temp = globals.ranked
-
-#     total = 0
-#     for i in range(3):
-#         start = timer()
-#         columnar(cipherText)
-#         end = timer()
-#         total += end - start
-#     print(f'elapsed time(MultiThreaded): {mulithreaded/3}')
-#     print(f'elapsed time(Normal): {total/3}')
-
-#     print(temp==globals.ranked)
-#     # print(temp)
-#     # outputToConsole(100)
-
-#     # columnar("MNHERZRNTDHZAAUINEEYSDEZ")
 if __name__ == '__main__':
     freeze_support()
-    # columnarMultiThreaded("MNHERZRNTDHZAAUINEEYSDEZ")
